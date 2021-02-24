6b69f067bb5c889db200a2b81b893f04f207db90
Ankit-RV <53686812+Ankit-RV@users.noreply.github.com>
2021-02-24 12:24:10 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,33 +1,72 @@
 # geitplTest1
 '''1.Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included).The numbers obtained should be printed in a comma-separated sequence on a single line.'''
 
+for i in range(2000, 3201):
+    if i % 7 == 0 and i % 5 != 0:
+        print(i,end=',')
+        
+      
+'''2.Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line.Suppose the following input is supplied to the program: 8 Then, the output should be:40320'''
 
-'''2.Write a program which can compute the factorial of a given numbers.The results should be printed in a comma-separated sequence on a single line.Suppose the following input is supplied to the program: 8 Then, the output should be:40320'''
+fact = 1
+num = int(input("Enter the number: "))
 
+if(num < 0 or num==0):
+   print("Sorry")
+ else:
+     for i in range(1,num+1):
+         fact = fact*i
 
-'''3.With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.Suppose the following input is supplied to the program: 8
+ print(fact)
 
-Then, the output should be:
-
+'''3.With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an integral number between 1 and n (both included).
+and then the program should print the dictionary.Suppose the following input is supplied to the program: 8Then, the output should be:
 {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}'''
 
+ dic = {}
+ a = int(input("Enter the number: "))
 
-'''4.Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.Suppose the following input is supplied to the program:
 
-34,67,55,33,12,98
+for i in range(1,a+1):
+     dic[i] = i*i
+ print(dic)
+
+
+
+'''4.Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
+Suppose the following input is supplied to the program:34,67,55,33,12,98
 Then, the output should be:
-
 ['34', '67', '55', '33', '12', '98']
 ('34', '67', '55', '33', '12', '98')'''
 
+ li = input("Enter").split(",")
+ tup = tuple(li)
+ print(li)
+ print(tup)
+
+take_input = input('Type the nubers in list with comma:  ')
+li = list(take_input.split(','))
+tup = tuple(take_input.split(','))
+print(li)
+print(tup)
 
 
 
 '''5.Define a class which has at least two methods:
-
 getString: to get a string from console input
 printString: to print the string in upper case.
 Also please include simple test function to test the class methods.'''
+
+class stringcontest: 
+  def __init__(self,x):
+         self.x = x
+      
+  def getstring(self):
+      print(self.x)
+      
+  def printString(self):
+      print(self.x.upper())
 
 
 
